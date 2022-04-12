9255a5d4d48dee4ee99ce2b4f9bdbdfd3c586b00
Long Vu <long.vu@sunbytes.vn>
2022-04-12 17:05:52 +0700

    test

--- script_test.py
+++ script_test.py
@@ -1,7 +1,7 @@
-import script
+import script_test
 
 def test_hello_world(capsys):
-    script.hello_world()
+    script_test.hello_world()
     out, err = capsys.readouterr()
 
     assert out == 'hello world\n'
